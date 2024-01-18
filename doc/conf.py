cd654dd20e876909ba02adddd0b27a629fd897c5
Robert Lieck <robert.lieck@durham.ac.uk>
2024-01-18 19:32:03 +0000

    add rename script

--- doc/conf.py
+++ doc/conf.py
@@ -18,7 +18,7 @@ sys.path.insert(0, os.path.abspath('../'))
 # -- Project information -----------------------------------------------------
 
 project = 'pythontemplatepackage'
-copyright = '2022, Robert Lieck'
+copyright = '2022 Robert Lieck'
 author = 'Robert Lieck'
 
 
