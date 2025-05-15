2eb7898a80fc08b8867e901b40f705dcb0208e91
Robert Lieck <robert.lieck@durham.ac.uk>
2025-05-15 14:53:30 +0200

    fix tests

--- .rename_test_dir/doc/conf.py
+++ .rename_test_dir/doc/conf.py
@@ -64,6 +64,9 @@ intersphinx_mapping = {}
 # report broken links
 nitpicky = True
 
+# include all Python files in example gallery (not just files starting with "plot_" as the default)
+sphinx_gallery_conf = {"filename_pattern": ''}
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
